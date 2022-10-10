b06e8177eaea396baa196ca3c5c5f625b8e1e8f0
Marcelo GAlvao <marcelogalvones@gmail.com>
2022-10-10 16:26:15 -0300

    teste unitario

--- src/app/pages/login/login.component.spec.ts
+++ src/app/pages/login/login.component.spec.ts
@@ -2,13 +2,17 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
 
-describe('LoginComponent', () => {
+import { Router } from '@angular/router';
+import { UsersService } from 'src/app/services/users.service';
+
+fdescribe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports: [Router, UsersService]
     })
     .compileComponents();
 
